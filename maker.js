cb972848e5704aeaaad80956eeec900c61689728
CoderCowMoo <broskichad38@gmail.com>
2021-06-11 11:36:05 +0300

    I've decided to use SQL and so I will have to teach myself jQuery and SQL. This project will take a while

---
+++ maker.js
@@ -0,0 +1,25 @@
+function onLoadFunction() {
+    // Here I will load up the data from timeline.json
+    // and create the relevant timeline elements.
+}
+
+function createNewElement(text, time) {
+    // This function handles the creation of a new timeline event
+}
+
+function takeInput() {
+    // Called when the user had inputted the time
+    // and text for the next timeline element
+}
+
+function deleteElement(elementID) {
+    /* Each elemen will be given an id so the json will
+       be stored like this:
+       Element1: {
+       'text': 'blabla',
+       'time': '1946',
+       'id': '1'
+       }
+       and given the element id, this function will 
+    */
+}       
